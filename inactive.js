d617951a1f2b109db0b23e0add97831b4f3bd36a
FozanGardezi <fozangardezi@gmail.com>
2021-03-13 20:31:27 +0500

    Add functionality to record in active users

--- inactive.js
+++ inactive.js
@@ -21,4 +21,18 @@ const inactiveThirdStage = () => {
 // this is the main method that acts like a worker and runs every 1 minute
 setInterval(function(){
     // please write your code here
+    
+    // change milliseconds to seconds
+    var now = new Date().getTime()/1000
+   
+    var minutesPassed = Math.floor((now - user.lastActivity/1000)/60)
+    
+    if (minutesPassed > 5 ) {
+        user.lastActivity = new Date().getTime()
+        inactiveThirdStage()
+    } else if (minutesPassed > 4 ) {
+        inactiveSecondStage()
+    } else if (minutesPassed > 2) {
+        inactiveFirstStage()
+    }
 }, 1000);
